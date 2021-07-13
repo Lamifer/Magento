361c02d9b26720738abc68e9a2053105fefdf411
Aamir Hussain <56257574+Lamifer@users.noreply.github.com>
2021-07-13 16:57:36 +0530

    Add files via upload

---
+++ Customer/CustomCheckout/Observer/CheckoutSubmitAllAfterObserver.php
@@ -0,0 +1,28 @@
+<?php
+namespace Customer\CustomCheckout\Observer;
+
+class CheckoutSubmitAllAfterObserver implements \Magento\Framework\Event\ObserverInterface
+{
+    /**
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return $this
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $order = $observer->getEvent()->getOrder();
+        $quote = $observer->getEvent()->getQuote();
+        if (empty($order) || empty($quote)) {
+            return $this;
+        }
+
+        $shippingAddress = $quote->getShippingAddress();
+        if ($shippingAddress->getDeliveryDate()) {
+            $orderShippingAddress = $order->getShippingAddress();
+            $orderShippingAddress->setDeliveryDate(
+                $shippingAddress->getDeliveryDate()
+            )->save();
+        }
+
+        return $this;
+    }
+}
---
+++ Customer/CustomCheckout/Plugin/Checkout/Model/ShippingInformationManagement.php
@@ -0,0 +1,25 @@
+<?php
+namespace Customer\CustomCheckout\Plugin\Checkout\Model;
+
+class ShippingInformationManagement
+{
+    /**
+     * @param \Magento\Checkout\Model\ShippingInformationManagement $subject
+     * @param $cartId
+     * @param \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+     */
+    public function beforeSaveAddressInformation(
+        \Magento\Checkout\Model\ShippingInformationManagement $subject,
+        $cartId,
+        \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+    ) {
+        $shippingAddress = $addressInformation->getShippingAddress();
+        $shippingExtensionAttributes = $shippingAddress->getExtensionAttributes();
+        if (!empty($shippingExtensionAttributes)) {
+            $shippingExtensionAttributes = $shippingAddress->getExtensionAttributes();
+            if (!empty($shippingExtensionAttributes)) {
+                $shippingAddress->setDeliveryDate($shippingExtensionAttributes->getDeliveryDate());
+            }
+        }
+    }
+}
